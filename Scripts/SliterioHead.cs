3d460a5cf9fb754cd87ea949d10bbe831854053f
DonmusBorek <109541782+DonmusBorek@users.noreply.github.com>
2022-11-29 00:26:20 +0300

    a

--- Scripts/SliterioHead.cs
+++ Scripts/SliterioHead.cs
@@ -3,18 +3,22 @@ using System;
 
 public class SliterioHead : KinematicBody2D
 {
-    public RigidBody2D popo;
-    
+    public KinematicBody2D player;
+    float speed = 0f;
+    Vector2 velocity;
+
     public override void _PhysicsProcess(float delta)
     {
-        this.GlobalPosition = GetGlobalMousePosition();
+        player = GetNode<KinematicBody2D>("../../Player");
 
-     //   Rotation = (GetGlobalMousePosition() - GlobalPosition).Angle();
-
-
-        //  this.GlobalRotation += (float)0.05;
-
-        Update();
-
+        if (player.Position.x < 0)
+        {
+            speed = -7f;
+        }
+        else
+        {
+            speed = 7f;
+        }
+        velocity = MoveAndSlide(new Vector2(speed* 50, velocity.y));
     }
 }
